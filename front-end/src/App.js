9b0b3f65c1cb7e31f18a9df691c8e8a0c09d9f84
shaifali <shaifali02000@gmail.com>
2023-03-29 19:31:41 +0530

    firstComitDAY2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Components/AllRoutes';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
